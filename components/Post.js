c52f451adea93b69512d0e88bd0e1af3f1223eb8
youssefraouf <youssefraouf32@gmail.com>
2020-01-28 17:50:27 +0200

    fixing three dots button in post

--- components/Post.js
+++ components/Post.js
@@ -4,23 +4,25 @@ import {AntDesign, EvilIcons, Ionicons,Entypo, FontAwesome} from '@expo/vector-i
 export function Post(props) {
   return (
       <View style={{backgroundColor:'#1F1F1F',paddingTop:5}}>
-       <View style={{marginLeft:10,height:50,flexDirection:'row',}}>
+       <View style={{marginLeft:10,height:50,flexDirection:'row',flex:1}}>
         <View>
             <Image source={{uri:'https://filedn.com/ltOdFv1aqz1YIFhf4gTY8D7/ingus-info/BLOGS/Photography-stocks3/stock-photography-slider.jpg'}}
             style={{height:50,width:50,borderRadius:25}}/>
         </View>
         <View style={{marginLeft:10,flexDirection:'column'}}>
-          <Text style={{fontSize:18,color:'white'}}>{props.user_name}</Text>
-          <Text style={{color:'#555555'}}>{props.created_at}</Text>    
+          <Text style={{fontSize:18,color:'white'}}>{props.post.user.name}</Text>
+          <Text style={{color:'#555555'}}>{props.post.created_at}</Text>    
         </View>
-       <TouchableOpacity style={{alignItems:'flex-end',flexGrow:1,marginRight:10,marginTop:0}}>        
+        <View style={{flexDirection:'row-reverse',flex:1}}>
+       <TouchableOpacity style={{alignItems:'flex-end',marginRight:10,marginTop:0}}>        
         <Entypo style={{fontSize:25,color:'#555555'}} name="dots-three-vertical"/>
         </TouchableOpacity>
+        </View>
        </View>
-        <Text style={{marginLeft:10,marginTop:5,fontSize:15,color:'white'}}>{props.text}</Text>
+        <Text style={{marginLeft:10,marginTop:5,fontSize:15,color:'white'}}>{props.post.text}</Text>
         <Image source={{uri:'https://filedn.com/ltOdFv1aqz1YIFhf4gTY8D7/ingus-info/BLOGS/Photography-stocks3/stock-photography-slider.jpg'}}
             style={{height:400}}/>
-        <View style={{flexDirection:'row',justifyContent:'space-between',height:20,marginTop:5}}>
+        <View style={{flexDirection:'row',justifyContent:'space-between',height:20,marginTop:5,marginLeft:10,marginRight:10}}>
         <Text style={{color:'white'}}>19 likes</Text>
         <Text style={{color:'white'}}>19 comments</Text>
         </View>
@@ -44,8 +46,7 @@ export function Post(props) {
           </TouchableOpacity>
         </View>
        </View>
-       <View style={{height:20,backgroundColor:'#161616',marginBottom:10}}>
-
+       <View style={{height:18,backgroundColor:'#161616',marginBottom:10}}>
         </View>
     </View>
   );
